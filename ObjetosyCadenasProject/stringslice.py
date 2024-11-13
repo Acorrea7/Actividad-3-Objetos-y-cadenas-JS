614f9bf224d39f8b9520d68695be7214be78920e
Andres Correa <andrescorrea0599@gmail.com>
2024-11-13 09:17:12 -0500

    Se realiza el metodo String slice()

---
+++ ObjetosyCadenasProject/stringslice.py
@@ -0,0 +1,12 @@
+# Cadena original
+cadena = "Hola, mundo"
+
+# Usando slice() para extraer "Hola"
+saludo = cadena[0:4]
+
+print(saludo)  # Salida: "Hola"
+
+# Usando slice() para extraer "mundo"
+mundo = cadena[6:]
+
+print(mundo)  # Salida: "mundo"
