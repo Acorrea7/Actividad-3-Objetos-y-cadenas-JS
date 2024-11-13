6c5770dd6411568c1731e4be11630307365d0a68
Angie Vivas <angievivas1107@gmail.com>
2024-11-13 18:49:41 -0500

    se crean los metodos string y string.slice

---
+++ ObjetosyCadenasProject/recursos/js/string.js
@@ -0,0 +1,4 @@
+let frutas = ["manzana", "banana", "pera", "uva"];
+
+document.getElementById("demo").innerHTML = frutas;
+window.alert(frutas);
---
+++ ObjetosyCadenasProject/recursos/js/string.slice.js
@@ -0,0 +1,8 @@
+let saludo = "Hola, mundo!";
+let subcadena = saludo.slice(1, 5); // Extrae "ola,"
+console.log(subcadena); // Imprime: ola,
+
+document.getElementById("demo1").innerHTML = saludo;
+window.alert(saludo);
+document.getElementById("demo2").innerHTML = subcadena;
+window.alert(subcadena);
